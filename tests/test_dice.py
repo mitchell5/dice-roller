08998133e8019531f9fe1df327736f2049c69c93
denizozkaymak <denizgokalp.5@gmail.com>
2025-02-17 13:59:52 +0000

    Revert "Fix API always returning 0 with TDD."
    This reverts commit dce4b27359a6bc19931ec32e180cf30cfc2f2036.

--- tests/test_dice.py
+++
@@ -1,9 +0,0 @@
-from dice_roller.dice import Dice
-
-class TestDice():
-    def test_one_roll(self):
-        dice = Dice()
-        roll = dice.roll()
-        assert type(roll) == int
-        assert roll > 0
-        assert roll < 7
