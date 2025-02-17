9e21f54c66fec1d936b607d327c6efaf00265881
denizozkaymak <denizgokalp.5@gmail.com>
2025-02-17 11:28:30 +0000

    First commit with API always returning 0

---
+++ dice_roller/dice.py
@@ -0,0 +1,8 @@
+class Dice:
+    def roll(self):
+        return 0
+
+
+if __name__=="__main__":
+    dice = Dice()
+    print(dice.roll())
---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# wsgi.py
+from flask import Flask, jsonify
+from dice_roller.dice import Dice
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    dice = Dice()
+    return jsonify({'roll': dice.roll()})
