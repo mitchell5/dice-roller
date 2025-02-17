60290de61a6845d40935712374f718b35ff4a296
denizozkaymak <denizgokalp.5@gmail.com>
2025-02-17 15:41:41 +0000

    Fixing poetry dependencies.

--- dice_roller/dice.py
+++ dice_roller/dice.py
@@ -1,12 +1,7 @@
 import random
 class Dice:
     def roll(self):
-<<<<<<< HEAD
-        return random.randint(1, 6)
-=======
         return random.randint(1,6)
->>>>>>> 37945a6115d52e18ce06694755e17fef33bc9edf
-
 
 if __name__=="__main__":
     dice = Dice()
